8cc6892bcd9292d6d4a527a54c811baeedbab5e8
Amaya Switch <aswitch@drury.edu>
2025-01-24 14:06:12 -0600

    Lab 1 done

--- edit_me.py
+++ edit_me.py
@@ -1,15 +1,18 @@
-
+# lab 1 
+# Amaya Switch 
 def find_max(lst):
     """return the maximum element"""
 
     current_max = lst[0]
+    
+    for num in lst:
+        if num > current_max:
+            current_max = num
 
-    #
-    #  YOUR CODE GOES HERE
-    #
 
-    return current_max
+    return current_max    
 
 test_list = [2112*i % 2024 for i in range(10000)]
 
 print(find_max(test_list))
+
